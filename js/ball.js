d15929a2b6519f811704efe854f10537b67dc107
Asif Ali <asephkhan@gmail.com>
2022-02-03 14:34:02 +0000

    obstacles are random

--- js/ball.js
+++ js/ball.js
@@ -1,45 +1,52 @@
 class Ball {
-  constructor(game, x, y) {
-    this.game = game;
-    this.x = x;
-    this.y = y;
-    /* this.width = width;
-        this.height = height; */
-    this.img = new Image();
-    this.canvas;
-    this.color = 'red';
+    constructor(game, x, y) {
+      this.game = game;
+      this.x = x;
+      this.y = y;
+      /* this.width = width;
+          this.height = height; */
+      this.img = new Image();
+      this.canvas;
+      this.color = 'red';
+    }
+    left() {
+      return this.x - 30;
+    }
+    right() {
+      return this.x + 30;
+    }
+  
+    top() {
+      return this.y - 30;
+    }
+    bottom() {
+      return this.y + 30;
+    }
+  
+    crashWith(obstacle) {
+      return !(
+        this.bottom() < obstacle.top() ||
+        this.top() > obstacle.bottom() ||
+        this.right() < obstacle.left() ||
+        this.left() > obstacle.right()
+      );
+    }
+  
+    passedThrough(obstacle) {
+      return (
+        this.left() === obstacle.right() + 3
+      )
+    }
+  
+  
+    drawBall() {
+      this.game.ctx.beginPath();
+      this.game.ctx.arc(this.x, this.y, 30, Math.PI * 2, false);
+      this.game.ctx.fillStyle = this.color;
+      this.game.ctx.fill();
+      this.game.ctx.closePath();
+    }
+  
+  
   }
-  left() {
-    return this.x - 30;
-  }
-  right() {
-    return this.x + 30;
-  }
-
-  top() {
-    return this.y - 30;
-  }
-  bottom() {
-    return this.y + 30;
-  }
-
-  crashWith(obstacle) {
-    return !(
-      this.bottom() < obstacle.top() ||
-      this.top() > obstacle.bottom() ||
-      this.right() < obstacle.left() ||
-      this.left() > obstacle.right()
-    );
-  }
-
-  drawBall() {
-    this.game.ctx.beginPath();
-    this.game.ctx.arc(this.x, this.y, 30, Math.PI * 2, false);
-    this.game.ctx.fillStyle = this.color;
-    this.game.ctx.fill();
-    this.game.ctx.closePath();
-  }
-
-
-}
 
--- js/game.js
+++ js/game.js
@@ -1,124 +1,116 @@
 class Game {
-  constructor() {
-    this.canvas = document.getElementById("canvas");
-    this.ctx = this.canvas.getContext("2d");
-    this.ball = null;
-    this.obstacles = [];
-    this.background = new Image();
-    this.frames = 0;
-    this.x = 0;
-    this.y = 0;
-    this.canvasWidth = 1200;
-    this.canvasHeight = 600;
-    this.intervalId = null;
-    this.score = 0;
-    this.color = ['red', 'blue', 'green', 'yellow'];
-    this.obstaclesY = [0, 150, 300, 450]
-    this.isPassing = false;
-  }
-
-  start() {
-    //this will trigger only once
-    this.ball = new Ball(this, 200, 500);
-    const controls = new Controls(this);
-    controls.keyboardEvents();
-
-    //this will run at 60 fps
-    this.intervalId = setInterval(() => {
-      this.update();
-    }, 1000 / 60);
-  }
-
-  update() {
-    this.drawBackground();
-    this.ball.drawBall();
-    this.createObstacles();
-    this.obstacles.forEach((obstacle) => {
-      obstacle.x--;
-      obstacle.draw();
-      if (this.ball.crashWith(obstacle)) {
-        if (this.ball.color !== obstacle.color) {
-          this.stop();
-        } else {
-          console.log("right color");
-          this.score++;
-          setTimeout(() => {
-            this.ball.color = this.color[Math.floor(Math.random() * this.color.length)];
-
-          }, 2000);
-          
-          this.isPassing = true;
-          
-          console.log(this.score);
-        }
-      } /* else {
-         if (this.isPassing && obstacle.color === this.ball.color){
-            this.isPassing = false;
-            this.ball.color = this.color[1];
-            this.score++;
-
-         }
-      } */
-    });
-    this.frames++;
-    this.drawScore();
-    //this.checkGameOver();
-  }
-
-  drawBackground() {
-    this.background.src = "../images/background-image.jpg";
-    this.ctx.drawImage(
-      this.background,
-      this.x,
-      this.y,
-      this.canvasWidth,
-      this.canvasHeight
-    );
-  }
-  createObstacles() {
-    if (this.frames % 400 === 0) {
-      //copying the arrays to not change the originals
-      let colorsArray = [...this.color];
-      let indexArray = [...this.obstaclesY]
-  
-      //iterating for the colors/indexes
-      this.color.forEach((color) => {
-        //Random number for each array
-        let colorIndex = Math.floor(Math.random()* colorsArray.length );
-        let yIndex = Math.floor(Math.random()* indexArray.length)
-        
-        //pass the random value in the obstacle
-        this.obstacles.push(new Obstacle(this, colorsArray[colorIndex], indexArray[yIndex]));
-
-        //take out the randoms to not get repeats
-        indexArray.splice(colorIndex, 1);
-        colorsArray.splice(yIndex, 1);
-
-      })
-/*       this.obstacles.push(new Obstacle(this, "red", 0));
-      this.obstacles.push(new Obstacle(this, "green", 150));
-      this.obstacles.push(new Obstacle(this, "blue", 300));
-      this.obstacles.push(new Obstacle(this, "yellow", 450)); */
-    }
-  }
-
-   checkGameOver() {
-    const ball = this.ball;
-    let ballColor = "";
-    const crashed = this.obstacles.some(function (obstacle) {
-      ballColor = obstacle.color;
-      return ball.crashWith(obstacle);
-    }); 
-  }
-
-  drawScore() {
-    //let score = Math.floor(this.frames);
-    this.ctx.font = "32px serif";
-    this.ctx.fillStyle = "white";
-    this.ctx.fillText(`Score: ${this.score}`, 100, 30);
-  }
-
-  stop() {
-    clearInterval(this.intervalId);
-  }
-}
+   constructor() {
+     this.canvas = document.getElementById("canvas");
+     this.ctx = this.canvas.getContext("2d");
+     this.ball = null;
+     this.obstacles = [];
+     this.background = new Image();
+     this.frames = 0;
+     this.x = 0;
+     this.y = 0;
+     this.canvasWidth = 1200;
+     this.canvasHeight = 600;
+     this.intervalId = null;
+     this.score = 0;
+     this.color = ['red', 'blue', 'green', 'yellow'];
+     this.obstaclesY = [0, 150, 300, 450]
+     this.isPassing = false;
+     this.passedObstacle = false;
+   }
+ 
+   start() {
+     //this will trigger only once
+     this.ball = new Ball(this, 200, 400);
+     const controls = new Controls(this);
+     controls.keyboardEvents();
+ 
+     //this will run at 60 fps
+     this.intervalId = setInterval(() => {
+       this.update();
+     }, 1000 / 60);
+   }
+ 
+   update() {
+     this.drawBackground();
+     this.ball.drawBall();
+     this.createObstacles();
+     this.obstacles.forEach((obstacle) => {
+       obstacle.x--;
+       obstacle.draw();
+ 
+       if(this.ball.passedThrough(obstacle)) {
+         this.passedObstacle = true;      
+         this.ball.color = this.color[Math.floor(Math.random() * this.color.length)];
+         this.score += 0.25;
+       }
+ 
+       if (this.ball.crashWith(obstacle)) {
+         if (this.ball.color !== obstacle.color) {
+           this.stop();
+         }        
+           this.isPassing = true;
+       }
+     });
+     this.frames++;
+     this.drawScore();
+     console.log(this.passedObstacle);
+     
+   }
+ 
+   drawBackground() {
+     this.background.src = "../images/background-image.jpg";
+     this.ctx.drawImage(
+       this.background,
+       this.x,
+       this.y,
+       this.canvasWidth,
+       this.canvasHeight
+     );
+   }
+   createObstacles() {
+     if (this.frames % 400 === 0) {
+       //copying the arrays to not change the originals
+       let colorsArray = [...this.color];
+       let indexArray = [...this.obstaclesY]
+   
+       //iterating for the colors/indexes
+       this.color.forEach((color) => {
+         //Random number for each array
+         let colorIndex = Math.floor(Math.random() * colorsArray.length );
+         let yIndex = Math.floor(Math.random() * indexArray.length)
+         
+         //pass the random value in the obstacle
+         this.obstacles.push(new Obstacle(this, colorsArray[colorIndex], indexArray[yIndex]));
+ 
+         //take out the randoms to not get repeats
+         indexArray.splice(yIndex, 1);
+         colorsArray.splice(colorIndex, 1);
+ 
+       })
+ /*       this.obstacles.push(new Obstacle(this, "red", 0));
+       this.obstacles.push(new Obstacle(this, "green", 150));
+       this.obstacles.push(new Obstacle(this, "blue", 300));
+       this.obstacles.push(new Obstacle(this, "yellow", 450)); */
+     }
+   }
+ 
+    checkGameOver() {
+     const ball = this.ball;
+     let ballColor = "";
+     const crashed = this.obstacles.some(function (obstacle) {
+       ballColor = obstacle.color;
+       return ball.crashWith(obstacle);
+     }); 
+   }
+ 
+   drawScore() {
+     //let score = Math.floor(this.frames);
+     this.ctx.font = "32px serif";
+     this.ctx.fillStyle = "white";
+     this.ctx.fillText(`Score: ${this.score}`, 100, 30);
+   }
+ 
+   stop() {
+     clearInterval(this.intervalId);
+   }
+ }
